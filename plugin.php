24087372df71f0788cf483115ceebdd80c7e59dc
Qrac <qrac.jp@gmail.com>
2021-11-12 16:21:19 +0900

    Version

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Stop Auto Update Emails
 Plugin URI: https://wordpress.org/plugins/stop-auto-update-emails/
 Description: Add the function to stop automatic update emails to WordPress.
-Version: 0.0.1
+Version: 1.0.0
 Author: Qrac
 Author URI: https://qrac.jp
 Text Domain: stop-auto-update-emails
